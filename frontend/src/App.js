bb60b9aaeaa802523d84ab1ad26445e1170e41d4
carlos881206 <lrdsquirtle@gmail.com>
2022-11-22 07:25:00 -0500

    fix frontend base_url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
-const BASE_URL = 'http://192.168.2.168:4000';
+const BASE_URL = 'http://localhost:4000';
 
 function App() {
     const [shifts, setShifts] = useState([]);
